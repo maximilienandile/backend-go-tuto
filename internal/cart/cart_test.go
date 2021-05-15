c469b6cec56100ac2eedf364bf9fbe0d26be306b
Maximilien Andile <maximilien@practical-go-lessons.com>
2021-05-15 09:42:42 +0000

    add unit tests

--- internal/cart/cart_test.go
+++ internal/cart/cart_test.go
@@ -9,27 +9,86 @@ import (
 )
 
 func TestCart_TotalPriceVATInc(t *testing.T) {
-	// GIVEN
-	items := []Item{
-		{
-			ID:               "42",
-			ShortDescription: "A pair of socks",
-			UnitPriceVATInc:  money.New(100, "EUR"),
-			UnitPriceVATExc:  money.New(50, "EUR"),
-			VAT:              money.New(50, "EUR"),
-			Quantity:         1,
-		},
-	}
-	cart := Cart{
-		ID:           "42",
-		CurrencyCode: "EUR",
-		Items:        items,
-	}
-	// WHEN
-	actualTotalPrice, err := cart.TotalPriceVATInc()
+	t.Run("nominal", func(t *testing.T) {
+		// GIVEN
+		items := []Item{
+			{
+				ID:               "42",
+				ShortDescription: "A pair of socks",
+				UnitPriceVATInc:  money.New(100, "EUR"),
+				UnitPriceVATExc:  money.New(50, "EUR"),
+				VAT:              money.New(50, "EUR"),
+				Quantity:         1,
+			},
+		}
+		cart := Cart{
+			ID:           "42",
+			CurrencyCode: "EUR",
+			Items:        items,
+		}
+		// WHEN
+		actualTotalPrice, err := cart.TotalPriceVATInc()
 
-	// THEN
-	assert.NoError(t, err, "impossible to compute total price VAT included")
-	expectedTotalPrice := money.New(100, "EUR")
-	assert.Equal(t, expectedTotalPrice, actualTotalPrice)
+		// THEN
+		assert.NoError(t, err, "impossible to compute total price VAT included")
+		expectedTotalPrice := money.New(100, "EUR")
+		assert.Equal(t, expectedTotalPrice, actualTotalPrice)
+	})
+	t.Run("nominal with quantity greater than 1 and 2 items", func(t *testing.T) {
+		// GIVEN
+		items := []Item{
+			{
+				ID:               "42",
+				ShortDescription: "A pair of socks",
+				UnitPriceVATInc:  money.New(100, "EUR"),
+				UnitPriceVATExc:  money.New(50, "EUR"),
+				VAT:              money.New(50, "EUR"),
+				Quantity:         1,
+			},
+			{
+				ID:               "43",
+				ShortDescription: "A T-Shirt with a small gopher",
+				UnitPriceVATInc:  money.New(3480, "EUR"),
+				UnitPriceVATExc:  money.New(2900, "EUR"),
+				VAT:              money.New(580, "EUR"),
+				Quantity:         2,
+			},
+		}
+		cart := Cart{
+			ID:           "42",
+			CurrencyCode: "EUR",
+			Items:        items,
+		}
+
+		// WHEN
+		actualTotalPriceVATInc, err := cart.TotalPriceVATInc()
+
+		// THEN
+		assert.NoError(t, err, "should have no error when total price VAT inc is computed")
+		expectedTotalPriceVATINC := money.New(7060, "EUR")
+		assert.Equal(t, expectedTotalPriceVATINC, actualTotalPriceVATInc)
+	})
+	t.Run("error case different currencies", func(t *testing.T) {
+		items := []Item{
+			{
+				ID:               "42",
+				ShortDescription: "A pair of socks",
+				UnitPriceVATInc:  money.New(100, "USD"),
+				UnitPriceVATExc:  money.New(50, "USD"),
+				VAT:              money.New(50, "USD"),
+				Quantity:         1,
+			},
+		}
+		cart := Cart{
+			ID:           "42",
+			CurrencyCode: "EUR",
+			Items:        items,
+		}
+
+		// WHEN
+		_, err := cart.TotalPriceVATInc()
+
+		// THEN
+		assert.Error(t, err, "when I add an item with a currency X to a basket of currency Y the method TotalPriceVATInc should fail")
+	})
 }
