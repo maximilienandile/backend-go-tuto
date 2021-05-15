90352b5d51bd60857d906ca6b29cce396ea65eec
Maximilien Andile <maximilien@practical-go-lessons.com>
2021-05-15 09:42:42 +0000

    improve unit test with mocking

--- go.mod
+++ go.mod
@@ -8,6 +8,9 @@ require (
 	github.com/aws/aws-sdk-go v1.41.14
 	github.com/awslabs/aws-lambda-go-api-proxy v0.11.0
 	github.com/gin-gonic/gin v1.7.4
+	github.com/golang/mock v1.6.0
+	github.com/satori/go.uuid v1.2.0
+	github.com/stretchr/testify v1.7.0
 )
 
 require (
@@ -16,7 +19,6 @@ require (
 	github.com/go-playground/locales v0.13.0 // indirect
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
-	github.com/golang/mock v1.6.0 // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
@@ -25,9 +27,6 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/satori/go.uuid v1.2.0 // indirect
-	github.com/stretchr/objx v0.3.0 // indirect
-	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
 	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
 	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -7,6 +7,10 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/maximilienandile/backend-go-tuto/internal/storage"
+
+	"github.com/golang/mock/gomock"
+
 	"github.com/maximilienandile/backend-go-tuto/internal/product"
 
 	"github.com/stretchr/testify/assert"
@@ -14,8 +18,12 @@ import (
 
 func TestServer_CreateProduct(t *testing.T) {
 	// GIVEN
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockedStorage := storage.NewMockStorage(ctrl)
 	testServer, err := New(Config{
-		Port: 8080,
+		Port:    8080,
+		Storage: mockedStorage,
 	})
 	assert.NoError(t, err, "building a server should not return an error")
 	recorder := httptest.NewRecorder()
@@ -27,10 +35,13 @@ func TestServer_CreateProduct(t *testing.T) {
 	req, err := http.NewRequest("POST", "/admin/products", bytes.NewReader(inputProductJSON))
 	assert.NoError(t, err, "no error should happend when building the request")
 	req.Header.Add("Authorization", "ABC")
+	// mocks expectations
+	mockedStorage.EXPECT().CreateProduct(gomock.Any()).Return(nil)
 
 	// WHEN
 	testServer.Engine.ServeHTTP(recorder, req)
 
 	// THEN
 	assert.Equal(t, http.StatusOK, recorder.Code)
+	assert.Equal(t, "{\"id\":\"2cdd32ee-f855-463a-a9dd-fe97f760c3d8\",\"name\":\"test product\",\"image\":\"\",\"shortDescription\":\"\",\"description\":\"\",\"priceVatExcluded\":{\"money\":null,\"display\":\"\"},\"vat\":{\"money\":null,\"display\":\"\"},\"totalPrice\":{\"money\":null,\"display\":\"\"}}", recorder.Body.String())
 }
