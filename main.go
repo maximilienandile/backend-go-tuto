4bc068d7e17f5f3f0829f04518541641cbea2287
Maximilien Andile <maximilien@practical-go-lessons.com>
2021-05-15 09:42:42 +0000

    Initialization server

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("Hello World")
-	fmt.Println(time.Now())
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"hello": "world",
+		})
+	})
+	r.Run(":9090") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
