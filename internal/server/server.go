6aa5ef9ef7556160924fae0dd0dfdfb20df723d9
Maximilien Andile <maximilien@practical-go-lessons.com>
2021-05-15 09:42:42 +0000

    refactor done before port number

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,18 +3,32 @@ package server
 import (
 	"net/http"
 
+	"github.com/Rhymond/go-money"
+	"github.com/maximilienandile/backend-go-tuto/internal/product"
+
 	"github.com/gin-gonic/gin"
 	"github.com/maximilienandile/backend-go-tuto/internal/category"
 )
 
 type Server struct {
+	engine *gin.Engine
 }
 
 type Config struct {
 }
 
 func New(config Config) (*Server, error) {
-	return &Server{}, nil
+	engine := gin.Default()
+	s := &Server{
+		engine: engine,
+	}
+	engine.GET("/categories", s.Categories)
+	engine.GET("/products", s.Products)
+	return s, nil
+}
+
+func (s *Server) Run() error {
+	return s.engine.Run(":9090")
 }
 
 func (s *Server) Categories(c *gin.Context) {
@@ -32,4 +46,32 @@ func (s *Server) Categories(c *gin.Context) {
 	}
 	c.Header("Access-Control-Allow-Origin", "http://localhost:8080")
 	c.JSON(http.StatusOK, categories)
+}
+
+func (s *Server) Products(c *gin.Context) {
+	products := []product.Product{
+		{
+			ID:               "42",
+			Name:             "Test",
+			Description:      "This is my product",
+			PriceVATExcluded: money.New(100, "EUR"),
+			VAT:              money.New(200, "EUR"),
+		},
+		{
+			ID:               "43",
+			Name:             "Test",
+			Description:      "This is my product",
+			PriceVATExcluded: money.New(100, "EUR"),
+			VAT:              money.New(250, "EUR"),
+		},
+		{
+			ID:               "44",
+			Name:             "Test",
+			Description:      "This is my product",
+			PriceVATExcluded: money.New(189, "EUR"),
+			VAT:              money.New(200, "EUR"),
+		},
+	}
+	c.Header("Access-Control-Allow-Origin", "http://localhost:8080")
+	c.JSON(200, products)
 }
--- main.go
+++ main.go
@@ -4,53 +4,15 @@ import (
 	"log"
 
 	"github.com/maximilienandile/backend-go-tuto/internal/server"
-
-	"github.com/Rhymond/go-money"
-	"github.com/gin-gonic/gin"
-	"github.com/maximilienandile/backend-go-tuto/internal/product"
 )
 
 func main() {
-
 	myServer, err := server.New(server.Config{})
 	if err != nil {
 		log.Fatalf("impossible to create the server: %s", err)
 	}
-
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"hello": "world",
-		})
-	})
-	r.GET("/categories", myServer.Categories)
-	// TODO : do the same as /categories for this endpoint
-	r.GET("/products", func(c *gin.Context) {
-		products := []product.Product{
-			{
-				ID:               "42",
-				Name:             "Test",
-				Description:      "This is my product",
-				PriceVATExcluded: money.New(100, "EUR"),
-				VAT:              money.New(200, "EUR"),
-			},
-			{
-				ID:               "43",
-				Name:             "Test",
-				Description:      "This is my product",
-				PriceVATExcluded: money.New(100, "EUR"),
-				VAT:              money.New(250, "EUR"),
-			},
-			{
-				ID:               "44",
-				Name:             "Test",
-				Description:      "This is my product",
-				PriceVATExcluded: money.New(189, "EUR"),
-				VAT:              money.New(200, "EUR"),
-			},
-		}
-		c.Header("Access-Control-Allow-Origin", "http://localhost:8080")
-		c.JSON(200, products)
-	})
-	r.Run(":9090") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	err = myServer.Run()
+	if err != nil {
+		log.Fatalf("impossible to start the server: %s", err)
+	}
 }
