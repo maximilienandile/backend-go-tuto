a2f14b1a993558966cd44884a8a9fc492344e280
Maximilien Andile <maximilien@practical-go-lessons.com>
2021-05-15 09:42:42 +0000

    get products

--- main.go
+++ main.go
@@ -14,13 +14,30 @@ func main() {
 		})
 	})
 	r.GET("/products", func(c *gin.Context) {
-		c.JSON(200, product.Product{
-			ID:               "42",
-			Name:             "Test",
-			Description:      "This is my product",
-			PriceVATExcluded: money.New(100, "EUR"),
-			VAT:              money.New(200, "EUR"),
-		})
+		products := []product.Product{
+			{
+				ID:               "42",
+				Name:             "Test",
+				Description:      "This is my product",
+				PriceVATExcluded: money.New(100, "EUR"),
+				VAT:              money.New(200, "EUR"),
+			},
+			{
+				ID:               "43",
+				Name:             "Test",
+				Description:      "This is my product",
+				PriceVATExcluded: money.New(100, "EUR"),
+				VAT:              money.New(250, "EUR"),
+			},
+			{
+				ID:               "44",
+				Name:             "Test",
+				Description:      "This is my product",
+				PriceVATExcluded: money.New(189, "EUR"),
+				VAT:              money.New(200, "EUR"),
+			},
+		}
+		c.JSON(200, products)
 	})
 	r.Run(":9090") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
