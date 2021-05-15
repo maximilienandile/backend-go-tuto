e26de29b97f254b24885cc18a3a18a886724733e
Maximilien Andile <maximilien@practical-go-lessons.com>
2021-05-15 09:42:42 +0000

    go 101 1 End

---
+++ application/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	userId := "4da1fa03"
+	fmt.Println(userId)
+
+	var cents uint64 = 325
+	fmt.Println(cents)
+
+	vatRate := 20.00
+	fmt.Println(vatRate)
+
+	var port int
+	fmt.Println(port)
+}
---
+++ testVar/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var quantity int
+	//fmt.Println(quantity) // 0
+	//quantity = 42
+	//fmt.Println(quantity) // 42
+	//var quantity,stockId = 42,52
+	//fmt.Println(quantity,stockId)
+
+	quantity := 42
+	fmt.Println(quantity)
+
+	var longitude int32
+	fmt.Println(longitude)
+	// 0
+	var cents uint8
+	fmt.Println(cents)
+	// 0
+	var hasOrdered bool
+	fmt.Println(hasOrdered)
+	// false
+	var firstname string
+	fmt.Println(firstname)
+	// ""
+	var percentage float32
+	fmt.Println(percentage)
+	// 0
+
+}
